e3ca242a841d903341cf5df3c698c782ca3de960
rajat380 <rajat2021october@gmail.com>
2026-02-15 23:15:03 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (12.56%) - LeetSync

---
+++ 67-add-binary/add-binary.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string ans = "";
+        int carry = 0;
+
+        int i = a.size() - 1;
+        int j = b.size() - 1;
+
+        for (; i >= 0 || j >= 0 || carry; i--, j--) {
+            int sum = carry;
+
+            if (i >= 0) sum += a[i] - '0';
+            if (j >= 0) sum += b[j] - '0';
+
+            ans = char(sum % 2 + '0') + ans;
+            carry = sum / 2;
+        }
+
+        return ans;
+    }
+};
