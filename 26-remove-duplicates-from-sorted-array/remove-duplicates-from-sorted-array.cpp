46c0f18d3b30356d38861765b890acc20ff01a58
rajat380 <rajat2021october@gmail.com>
2026-06-06 11:31:19 +0530

    Time: 0 ms (100.00%) | Memory: 22.5 MB (97.68%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        unordered_map<int, int> mp;
-        vector<int> temp;
 
-        for (int i = 0; i < nums.size(); i++) {
-            if (mp.find(nums[i]) == mp.end()) {
-                mp[nums[i]] = 1;
-                temp.push_back(nums[i]);
+        int k = 1;
+
+        for(int i = 1; i < nums.size(); i++) {
+            if(nums[i] != nums[i-1]) {
+                nums[k] = nums[i];
+                k++;
             }
         }
 
-        for (int i = 0; i < temp.size(); i++) {
-            nums[i] = temp[i];
-        }
-
-        return temp.size();
+        return k;
     }
 };
